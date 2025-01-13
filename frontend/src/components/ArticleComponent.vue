1a92fe8756c4ddeb2defea45f2e65ff259624dee
mcbauman <m.c.baumann@icloud.com>
2025-01-13 14:08:41 +0100

    ImageSolved

--- frontend/src/components/ArticleComponent.vue
+++ frontend/src/components/ArticleComponent.vue
@@ -1,23 +1,27 @@
 <script setup>
-const props = defineProps(["articleContent"])
+const props = defineProps(["articleContent"]);
 </script>
 <template>
-    <article class="articleBody">
-        <img v-if="articleContent.mainpicture" src="../assets/pictures/torgeFirstTraining2025.jpg" :alt="articleContent.mainpicture">
-        <div>
-        <header class="articleHeader"> 
-            <h3>
-                {{ articleContent.article.title }}
-            </h3>
-        </header>
-            <p>{{ articleContent.article.text }}</p>
-            <!-- <section v-for="sectionContent in articleContent.article.sections">
+  <article class="articleBody">
+    <!-- <img v-if="articleContent.mainpicture" src="../assets/pictures/torgeFirstTraining2025.jpg" :alt="articleContent.mainpicture"> -->
+    <img
+      v-if="articleContent.mainpicture"
+      v-bind:src="articleContent.mainpicture"
+      :alt="articleContent.mainpicture"
+    />
+    <div>
+      <header class="articleHeader">
+        <h3>
+          {{ articleContent.article.title }}
+        </h3>
+      </header>
+      <p>{{ articleContent.article.text }}</p>
+      <!-- <section v-for="sectionContent in articleContent.article.sections">
                 <h4>{{ sectionContent.title }}</h4>
                 <img v-if="sectionContent.picture" :src="sectionContent.picture" alt="">
                 <p>{{ sectionContent.text }}</p>
             </section> -->
-        </div>
-    </article>
-    <hr>
-    <!-- {{ articleContent }} -->
+    </div>
+  </article>
+  <!-- {{ articleContent }} -->
 </template>
--- frontend/src/stores/articles.js
+++ frontend/src/stores/articles.js
@@ -2,7 +2,7 @@ export const articles=[
     {
         date:"12.01.2025",
         kategorie:"news",
-        mainpicture:"../assets/pictures/torgeFirstTraining2025.jpg",
+        mainpicture:"../../public/pictures/torgeFirstTraining2025.jpg",
         article:{
             title:"first Sunshine, first training",
             text:"Am ersten Wochenende mit richtig schönem Wetter hieß es für uns direkt ab auf's Rad, erstes Training für die große April-Fahrrad-Reise",
@@ -18,7 +18,7 @@ export const articles=[
     {
         date:"13.01.2025",
         kategorie:"technik",
-        mainpicture:"../assets/pictures/torgeFirstTraining2025.jpg",
+        mainpicture:"../../public//pictures/torgeFirstTraining2025.jpg",
         article:{
             title:"Alles um Nabenschaltungen",
             text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
