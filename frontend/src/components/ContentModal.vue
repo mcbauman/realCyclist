33c770d546ef76c6e79d9502ebfa579fd9b717ed
mcbauman <m.c.baumann@icloud.com>
2025-01-15 13:45:37 +0100

    FlexModal

--- frontend/src/components/ContentModal.vue
+++ frontend/src/components/ContentModal.vue
@@ -7,13 +7,15 @@ import LeaveIcon from '@/assets/icons/LeaveIcon.vue';
     <article id="contentModalBackground">
         <section id="contentModal">
             <header class="articleHeader">
-                <h1>{{settingsStore.modalContent.article.title}}</h1>
+                <h2>{{settingsStore.modalContent.article.title}}</h2>
                 <button @click.prevent="settingsStore.setModalContent(false)"><LeaveIcon /></button>
             </header>
-            <section v-for="paragraph in settingsStore.modalContent.article.sections">
-                <h2>{{ paragraph.title }}</h2>
+            <section class="modalParagraph" v-for="paragraph in settingsStore.modalContent.article.sections">
                 <img v-if="paragraph.picture" v-bind:src="paragraph.picture" alt="paragraph.picture">
-                <p>{{ paragraph.text }}</p>
+                <span>
+                    <h3>{{ paragraph.title }}</h3>
+                    <p>{{ paragraph.text }}</p>
+                </span>
             </section>
         </section>
     </article>
