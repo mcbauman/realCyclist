6ac5819e11e066b7c40afd4fdd9a460756d2c988
mcbauman <m.c.baumann@icloud.com>
2025-02-03 13:01:05 +0100

    JustContent

--- frontend/src/stores/articles.js
+++ frontend/src/stores/articles.js
@@ -1,146 +1,161 @@
-export const articles=[
-    {
-        date:"12.01.2025",
-        kategorie:"reise",
-        mainpicture:"../../public/pictures/torgeFirstTraining2025.jpg",
-        article:{
-            title:"first Sunshine, first training",
-            text:"Am ersten Wochenende mit richtig schönem Wetter hieß es für uns direkt ab auf's Rad, erstes Training für die große April-Fahrrad-Reise",
-            sections:[
-                {
-                    title:"Einordnung",
-                    picture:null,
-                    text:" Seit 2024 sind wir auf unserer Europa-Radreise. Also nicht die ganze Zeit - aber immer wieder. Wegen Arbeit und Schule können wir ja nur in den Ferien Reisen. Um den Traum der Europa-Radreise trotzdem zu realisieren, haben wir uns entschlossen, die Reise in Etappen zu vollziehen. Mit dem Rad ging die erste Etappe nach Amsterdam, von Dort aus gings mit dem Zug zurück nach Hause. Die zweite Etappe wird also in Amsterdam starten und so weiter ... "
-                }
-            ]
-        }
+export const articles = [
+  {
+    date: "12.01.2025",
+    kategorie: "reise",
+    mainpicture: "../../public/pictures/torgeFirstTraining2025.jpg",
+    article: {
+      title: "first Sunshine, first training",
+      text: "Am ersten Wochenende mit richtig schönem Wetter hieß es für uns direkt ab auf's Rad. Das erstes Training für die große April-Fahrrad-Reise war ein Genuss.",
+      sections: [
+        {
+          title: "Einordnung",
+          picture: "../../public/pictures/torgeFirstTraining2025.jpg",
+          text: " Seit 2024 sind wir auf unserer Europa-Radreise. Also nicht die ganze Zeit - aber immer wieder. Wegen Arbeit und Schule können wir ja nur in den Ferien Reisen. Um den Traum der Europa-Radreise trotzdem zu realisieren, haben wir uns entschlossen, die Reise in Etappen zu vollziehen. Mit dem Rad ging die erste Etappe nach Amsterdam, von Dort aus gings mit dem Zug zurück nach Hause. Die zweite Etappe wird also in Amsterdam starten und so weiter ... ",
+        },
+      ],
     },
-    {
-        date:"13.01.2025",
-        kategorie:"technik",
-        mainpicture:"../../public/pictures/dirtychain.jpg",
-        article:{
-            title:"Alles um Nabenschaltungen",
-            text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
-            sections:[
-                {
-                    title:"Wenn ist die Kettenschaltung mist ist",
-                    picture:"../../public/pictures/dirtychain.jpg",
-                    text:"Die Kettenschaltung ist von den Rahmendaten in vielen Bedingungen überlegen. Sie ist günstig, leicht und hat eine unübertroffene Effizienz. Bei schlechtem Wetter ist sie aber sehr Plfegeintensiv. Nach ner Stunde Radfahrt bei schlechtem Wetter muss sollte Sie komplett gereinigt und neu gefettet werden. Damit ist sie für den täglichen Einsatz nicht geeignet."
-                }
-            ]
-        }
+  },
+  {
+    date: "13.01.2025",
+    kategorie: "technik",
+    mainpicture: "../../public/pictures/dirtychain.jpg",
+    article: {
+      title: "Alles um Nabenschaltungen",
+      text: "Warum schwören manche Radler auf Nabenschaltungen, derweil sie an sportlichen Rädern die absolute Ausnahme sind? Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
+      sections: [
+        {
+          title: "Wenn ist die Kettenschaltung mist ist",
+          picture: "../../public/pictures/dirtychain.jpg",
+          text: "Die Kettenschaltung ist von den Rahmendaten in vielen Bedingungen überlegen. Sie ist günstig, leicht und hat eine unübertroffene Effizienz. Bei schlechtem Wetter ist sie aber sehr Plfegeintensiv. Nach ner Stunde Radfahrt bei schlechtem Wetter muss - bzw. sollte Sie komplett gereinigt und neu gefettet werden. Damit ist sie für den täglichen Einsatz nicht geeignet. Die Alternative bei Regen oder Schlechtwetter ist der Riemenantrieb. Der muss nach Regenfahrten in der Regel garnicht behandelt werden. Ab und zu eventuell doch mal mit wasser absprühlen wird aber auch nicht schaden. Nun setzt ein Riemenantrieb aber leider eine Nabenschaltung oder Getriebenabe vorraus.",
+        },
+        {
+            title: "Dynamik",
+            picture: null,
+            text: "Nabenschaltunten sind träge. Die ca. 2 Kilo einer Nabenschlatung sitzen im Hinterrad und müssen mitbeschleunigt werden. Damit ist jede Agilität beim Beschleunigen verschwunden. Die Ausnahme ist ist die Pinion Getriebeschaltung im Tretlager. Damit wird der Hinterreifen sogar noch wesentlich leichter, alls bei Kettenschaltungen. Während der Fahrt ist das gewicht dann wieder zu vernachlässigen. "
+         },
+         {
+            title: "Wirkungsgrad",
+            picture: null,
+            text: "Der Wirkungsgrad von Nabenschaltungen liegt leider relativ weit von Kettenschaltungen entfernt. Erschwerend kommt hinzu, dass der Leistungsverlust für die Mechanik der Nabe gerade in den Hohen gängen auch noch zunimmt. Hier geben Sie die untschiedlichen Modelle leider auch erschreckend wenig. Allein die Rohloff Nabe mit 14 Gängen sticht hier positiv aus der Masse hervor und kommt Kettenschaltungen schon recht nahe."
+         },
+         {
+            title: "Gänge und Abstufung",
+            picture: null,
+            text: "Derweil sich Kettenschaltungen sehr fein an die persönlichen Wünsch anpassen lassen, ist dies bei Nabenschaltungen nicht möglich. Freilich sind die Gangsprünge für viele Radler sehr gut. Im Sportlichen Bereich reichen Sie aber schnell nicht mehr aus. Möchte man zum Beispiel eine kurze Abstufung der höhehren Gänge und eine große Spreizung für die kleinen, bei Nabenschlatungen keine Chance. Die Entfaltungen sind zwar Teilweise sehr gut. Die Sprünge bei den Höheren Gängen aber viel zu groß."
+         },
+      ],
     },
-    {
-        date:"13.01.2025",
-        kategorie:"technik",
-        mainpicture:false,
-        article:{
-            title:"Alles um Nabenschaltungen",
-            text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
-            sections:[
-                {
-                    title:null,
-                    picture:null,
-                    text:"lorem ipsum "
-                }
-            ]
-        }
+  },
+  {
+    date: "13.01.2025",
+    kategorie: "technik",
+    mainpicture: false,
+    article: {
+      title: "Alles um Nabenschaltungen",
+      text: "Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
+      sections: [
+        {
+          title: null,
+          picture: null,
+          text: "lorem ipsum ",
+        },
+      ],
     },
-    {
-        date:"13.01.2025",
-        kategorie:"produkte",
-        mainpicture:"../../public//pictures/torgeFirstTraining2025.jpg",
-        article:{
-            title:"Alles um Nabenschaltungen",
-            text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
-            sections:[
-                {
-                    title:null,
-                    picture:null,
-                    text:"lorem ipsum "
-                }
-            ]
-        }
+  },
+  {
+    date: "13.01.2025",
+    kategorie: "produkte",
+    mainpicture: "../../public//pictures/torgeFirstTraining2025.jpg",
+    article: {
+      title: "Alles um Nabenschaltungen",
+      text: "Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
+      sections: [
+        {
+          title: null,
+          picture: null,
+          text: "lorem ipsum ",
+        },
+      ],
     },
-    {
-        date:"13.01.2025",
-        kategorie:"technik",
-        mainpicture:"../../public//pictures/torgeFirstTraining2025.jpg",
-        article:{
-            title:"Alles um Nabenschaltungen",
-            text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
-            sections:[
-                {
-                    title:null,
-                    picture:null,
-                    text:"lorem ipsum "
-                }
-            ]
-        }
+  },
+  {
+    date: "13.01.2025",
+    kategorie: "technik",
+    mainpicture: "../../public//pictures/torgeFirstTraining2025.jpg",
+    article: {
+      title: "Alles um Nabenschaltungen",
+      text: "Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
+      sections: [
+        {
+          title: null,
+          picture: null,
+          text: "lorem ipsum ",
+        },
+      ],
     },
-    {
-        date:"13.01.2025",
-        kategorie:"technik",
-        mainpicture:"../../public//pictures/torgeFirstTraining2025.jpg",
-        article:{
-            title:"Alles um Nabenschaltungen",
-            text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
-            sections:[
-                {
-                    title:null,
-                    picture:null,
-                    text:"lorem ipsum "
-                }
-            ]
-        }
+  },
+  {
+    date: "13.01.2025",
+    kategorie: "technik",
+    mainpicture: "../../public//pictures/torgeFirstTraining2025.jpg",
+    article: {
+      title: "Alles um Nabenschaltungen",
+      text: "Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
+      sections: [
+        {
+          title: null,
+          picture: null,
+          text: "lorem ipsum ",
+        },
+      ],
     },
-    {
-        date:"13.01.2025",
-        kategorie:"technik",
-        mainpicture:"../../public//pictures/torgeFirstTraining2025.jpg",
-        article:{
-            title:"Alles um Nabenschaltungen",
-            text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
-            sections:[
-                {
-                    title:null,
-                    picture:null,
-                    text:"lorem ipsum "
-                }
-            ]
-        }
+  },
+  {
+    date: "13.01.2025",
+    kategorie: "technik",
+    mainpicture: "../../public//pictures/torgeFirstTraining2025.jpg",
+    article: {
+      title: "Alles um Nabenschaltungen",
+      text: "Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
+      sections: [
+        {
+          title: null,
+          picture: null,
+          text: "lorem ipsum ",
+        },
+      ],
     },
-    {
-        date:"13.01.2025",
-        kategorie:"technik",
-        mainpicture:"../../public//pictures/torgeFirstTraining2025.jpg",
-        article:{
-            title:"Alles um Nabenschaltungen",
-            text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
-            sections:[
-                {
-                    title:null,
-                    picture:null,
-                    text:"lorem ipsum "
-                }
-            ]
-        }
+  },
+  {
+    date: "13.01.2025",
+    kategorie: "technik",
+    mainpicture: "../../public//pictures/torgeFirstTraining2025.jpg",
+    article: {
+      title: "Alles um Nabenschaltungen",
+      text: "Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
+      sections: [
+        {
+          title: null,
+          picture: null,
+          text: "lorem ipsum ",
+        },
+      ],
     },
-    {
-        date:"13.01.2025",
-        kategorie:"technik",
-        mainpicture:"../../public//pictures/torgeFirstTraining2025.jpg",
-        article:{
-            title:"Alles um Nabenschaltungen",
-            text:"Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
-            sections:[
-                {
-                    title:null,
-                    picture:null,
-                    text:"lorem ipsum "
-                }
-            ]
-        }
-    }
-]
+  },
+  {
+    date: "13.01.2025",
+    kategorie: "technik",
+    mainpicture: "../../public//pictures/torgeFirstTraining2025.jpg",
+    article: {
+      title: "Alles um Nabenschaltungen",
+      text: "Viele Radler schwören auf Sie, an Rennrädern sind Sie die absolute Ausnahme: Nabenschaltungen. Für wen sind Sie die richtige Wahl? Wo liegen Vor- und Nachteile und welche Modelle sind interessant?",
+      sections: [
+        {
+          title: null,
+          picture: null,
+          text: "lorem ipsum ",
+        },
+      ],
+    },
+  },
+];
